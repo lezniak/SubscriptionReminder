3b9c69a824c08eb9a06cbf622920b538813dff98
Daniel <ddezersl@gmail.com>
2022-06-22 18:34:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/subscriptionreminder/ui/main/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.example.subscriptionreminder.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.subscriptionreminder.R
+import com.example.subscriptionreminder.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        binding = MainFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
