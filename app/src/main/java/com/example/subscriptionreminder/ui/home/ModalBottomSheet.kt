05641a5ed21dbce7594c32708e4785b7cee88b9f
Daniel <ddezersl@gmail.com>
2022-07-12 21:04:28 +0200

    bottom modal view

--- app/src/main/java/com/example/subscriptionreminder/ui/home/ModalBottomSheet.kt
+++ app/src/main/java/com/example/subscriptionreminder/ui/home/ModalBottomSheet.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.example.subscriptionreminder.databinding.BottomModalHomeBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import java.text.SimpleDateFormat
@@ -29,16 +30,22 @@ class ModalBottomSheet  : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val date =
-            DatePickerDialog.OnDateSetListener { view, year, month, day ->
-                myCalendar.set(Calendar.YEAR, year)
-                myCalendar.set(Calendar.MONTH, month)
-                myCalendar.set(Calendar.DAY_OF_MONTH, day)
-                updateLabel()
-            }
-        binding.date.setOnClickListener(View.OnClickListener {
+
+
+        binding.dateStart.setOnClickListener(View.OnClickListener {
             DatePickerDialog(
                 requireContext(),
-                date,
+                date(binding.dateStart),
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        })
+
+        binding.dateEnd.setOnClickListener(View.OnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                date(binding.dateEnd),
                 myCalendar.get(Calendar.YEAR),
                 myCalendar.get(Calendar.MONTH),
                 myCalendar.get(Calendar.DAY_OF_MONTH)
@@ -46,9 +53,18 @@ class ModalBottomSheet  : BottomSheetDialogFragment() {
         })
     }
 
-    private fun updateLabel() {
+    private fun updateLabel(editText: EditText) {
         val myFormat = "MM/dd/yy"
         val dateFormat = SimpleDateFormat(myFormat, Locale.US)
-        binding.date.setText(dateFormat.format(myCalendar.time))
+        editText.setText(dateFormat.format(myCalendar.time))
     }
+
+    private fun date(editText: EditText):DatePickerDialog.OnDateSetListener
+    = DatePickerDialog.OnDateSetListener { view, year, month, day ->
+        myCalendar.set(Calendar.YEAR, year)
+        myCalendar.set(Calendar.MONTH, month)
+        myCalendar.set(Calendar.DAY_OF_MONTH, day)
+        updateLabel(editText)
+    }
+
 }
